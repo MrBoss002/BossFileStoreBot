23187101f0742e8b94b579cddfefb52ee10cb819
MrBoss002 <127328495+MrBoss002@users.noreply.github.com>
2024-04-08 12:07:00 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-# (c) @AbirHasan2005
+# (c) @MrBoss002
 
 import os
 
@@ -22,7 +22,7 @@ class Config(object):
 This is Permanent Files Store Bot!
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
 
-🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
+🤖 **Iam:** [Boss Files Store Bot](https://t.me/{BOT_USERNAME})
 
 📝 **Language:** [Python3](https://www.python.org)
 
@@ -30,20 +30,20 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @MrBoss002
 
-👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
+👥 **Support:** [Click Here](https://t.me/MALLU_HUB_OFFICIAL)
 
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+📢 **Updates Channel:** [Boss Projects](https://t.me/Mallu_Hub_TG)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @MrBoss_002
 
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
+[Donate Now](https://t.me/Mallu_Hub_TG) (PayMent Methods)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
